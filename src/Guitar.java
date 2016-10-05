7d1eb01c5e58efc131e95078c7a7f37a78b949c2
t00148178 <tcc1011pm@yahoo.com>
2016-10-05 12:55:37 +0100

    Variables split into multiple declarations

--- src/Guitar.java
+++ src/Guitar.java
@@ -1,6 +1,11 @@
 public class Guitar {
 
-  private String serialNumber, builder, model, type, backWood, topWood;
+  private String serialNumber;
+  private String builder;
+  private String model;
+  private String type;
+  private String backWood;
+  private String topWood;
   private double price;
 
   public Guitar(String serialNumber, double price,
