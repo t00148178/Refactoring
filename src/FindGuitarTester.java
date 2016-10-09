3f10356ba2cdaf4ed9ea7d883405629938a2bb54
t00148178 <tcc1011pm@yahoo.com>
2016-10-09 16:40:23 +0100

    code refactored to work with enam class - Value Safety

--- src/FindGuitarTester.java
+++ src/FindGuitarTester.java
@@ -5,16 +5,17 @@ public class FindGuitarTester {
     Inventory inventory = new Inventory();
     initializeInventory(inventory);
 
-    Guitar whatErinLikes = new Guitar("", 0, "fender", "Stratocastor", 
-                                      "electric", "Alder", "Alder");
+    Guitar whatErinLikes = new Guitar("", 0, Builder.FENDER, "Stratocastor",
+            "electric", "Alder", "Alder");
     Guitar guitar = inventory.search(whatErinLikes);
     if (guitar != null) {
-      System.out.println("Erin, you might like this " +
-        guitar.getBuilder() + " " + guitar.getModel() +  " " +
-        guitar.getType() + " guitar:\n   " +
-        guitar.getBackWood() + " back and sides,\n   " +
-        guitar.getTopWood() + " top.\nYou can have it for only $" +
-        guitar.getPrice() + "!");
+      System.out.println("Nunber of matching guitars: " +
+              "\nErin, you might like this " +
+              guitar.getBuilder() + " " + guitar.getModel() +  " " +
+              guitar.getType() + " guitar:\n   " +
+              guitar.getBackWood() + " back and sides,\n   " +
+              guitar.getTopWood() + " top.\nYou can have it for only $" +
+              guitar.getPrice() + "!");
     } else {
       System.out.println("Sorry, Erin, we have nothing for you.");
     }
@@ -22,26 +23,26 @@ public class FindGuitarTester {
 
   private static void initializeInventory(Inventory inventory) {
     inventory.addGuitar("11277", 3999.95, "Collings", "CJ", "acoustic",
-                        "Indian Rosewood", "Sitka");
+            "Indian Rosewood", "Sitka");
     inventory.addGuitar("V95693", 1499.95, "Fender", "Stratocastor", "electric",
-                        "Alder", "Alder");
+            "Alder", "Alder");
     inventory.addGuitar("V9512", 1549.95, "Fender", "Stratocastor", "electric",
-                        "Alder", "Alder");
+            "Alder", "Alder");
     inventory.addGuitar("122784", 5495.95, "Martin", "D-18", "acoustic",
-                        "Mahogany", "Adirondack");
+            "Mahogany", "Adirondack");
     inventory.addGuitar("76531", 6295.95, "Martin", "OM-28", "acoustic",
-                        "Brazilian Rosewood", "Adriondack");
+            "Brazilian Rosewood", "Adriondack");
     inventory.addGuitar("70108276", 2295.95, "Gibson", "Les Paul", "electric",
-                        "Mahogany", "Maple");
+            "Mahogany", "Maple");
     inventory.addGuitar("82765501", 1890.95, "Gibson", "SG '61 Reissue",
-                        "electric", "Mahogany", "Mahogany");
+            "electric", "Mahogany", "Mahogany");
     inventory.addGuitar("77023", 6275.95, "Martin", "D-28", "acoustic",
-                        "Brazilian Rosewood", "Adirondack");
+            "Brazilian Rosewood", "Adirondack");
     inventory.addGuitar("1092", 12995.95, "Olson", "SJ", "acoustic",
-                        "Indian Rosewood", "Cedar");
+            "Indian Rosewood", "Cedar");
     inventory.addGuitar("566-62", 8999.95, "Ryan", "Cathedral", "acoustic",
-                        "Cocobolo", "Cedar");
+            "Cocobolo", "Cedar");
     inventory.addGuitar("6 29584", 2100.95, "PRS", "Dave Navarro Signature",
-                        "electric", "Mahogany", "Maple");
+            "electric", "Mahogany", "Maple");
   }
 }
--- src/Inventory.java
+++ src/Inventory.java
@@ -12,8 +12,8 @@ public class Inventory {
   public void addGuitar(String serialNumber, double price,
                         String builder, String model,
                         String type, String backWood, String topWood) {
-    Guitar guitar = new Guitar(serialNumber, price, builder,
-                               model, type, backWood, topWood);
+    Guitar guitar = new Guitar(serialNumber, price, Builder.FENDER,
+            model, type, backWood, topWood);
     guitars.add(guitar);
   }
   public Guitar getGuitar(String serialNumber) {
@@ -25,30 +25,32 @@ public class Inventory {
     }
     return null;
   }
+    
+
   public Guitar search(Guitar searchGuitar) {
     for (Iterator i = guitars.iterator(); i.hasNext(); ) {
       Guitar guitar = (Guitar)i.next();
       // Ignore serial number since that's unique
       // Ignore price since that's unique
-      String builder = searchGuitar.getBuilder();
+      Builder builder = searchGuitar.getBuilder();
       if ((builder != null) && (!builder.equals("")) &&
-          (!builder.equals(guitar.getBuilder())))
+              (!builder.equals(guitar.getBuilder())))
         continue;
       String model = searchGuitar.getModel();
       if ((model != null) && (!model.equals("")) &&
-          (!model.equals(guitar.getModel())))
+              (!model.equals(guitar.getModel())))
         continue;
       String type = searchGuitar.getType();
       if ((type != null) && (!searchGuitar.equals("")) &&
-          (!type.equals(guitar.getType())))
+              (!type.equals(guitar.getType())))
         continue;
       String backWood = searchGuitar.getBackWood();
       if ((backWood != null) && (!backWood.equals("")) &&
-          (!backWood.equals(guitar.getBackWood())))
+              (!backWood.equals(guitar.getBackWood())))
         continue;
       String topWood = searchGuitar.getTopWood();
       if ((topWood != null) && (!topWood.equals("")) &&
-          (!topWood.equals(guitar.getTopWood())))
+              (!topWood.equals(guitar.getTopWood())))
         continue;
       return guitar;
     }
